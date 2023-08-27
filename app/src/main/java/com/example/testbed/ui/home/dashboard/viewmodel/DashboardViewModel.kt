dc832a3c076cea89fc30ba6dc04f4dc65497975b
ansonkwan74 <59356349+ansonkwan74@users.noreply.github.com>
2023-08-27 22:34:50 +1000

    dagger hilt retrofit

--- app/src/main/java/com/example/testbed/ui/home/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/example/testbed/ui/home/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.testbed.ui.home.dashboard.viewmodel
 
-class DashboardViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DashboardViewModel @Inject constructor() : ViewModel(){
 }
